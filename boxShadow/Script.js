d20a9b0f38013c8155a7f506e07fe9b69c892847
Saksham <sakshamkhatri771@gmail.com>
2026-06-21 17:59:18 +0530

    boxShadow project

---
+++ boxShadow/Script.js
@@ -0,0 +1,25 @@
+    // addEventListener('mousemove',function(e){
+    //     document.body.style.setProperty("--x",e.clientX +'px');
+    //     document.body.style.setProperty("--y",e.clientY +'px');
+    // })
+
+    // var para = document.querySelector('p');
+    // var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    // var text = para.innerText
+    
+    // para.addEventListener('mouseenter', ()=>{
+    //     var iteration = 0;
+    // var a = setInterval(()=>{ 
+    //     const str = text.split('').map((char,index) =>{
+    //         if(index<iteration){
+    //             return char
+    //         }
+    //         return characters.split("")[Math.floor(Math.random()*53)]
+    //     }).join("")
+    //     console.log("hello")
+    //     para.innerText = str;
+    //     iteration++;
+    //     if(iteration>text.length) clearInterval(a)
+
+    // },50)
+    // })
